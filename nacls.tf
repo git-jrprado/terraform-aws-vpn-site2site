ba1baf5814cf99fc4ab8795ce1245999ad1603b8
José Roberto <31750084+git-jrprado@users.noreply.github.com>
2021-04-26 11:36:53 -0300

    Update nacls.tf

--- nacls.tf
+++ nacls.tf
@@ -1,6 +1,6 @@
 resource "aws_network_acl_rule" "in_vpn_s2s" {
   network_acl_id = var.nacl_table
-  rule_number    = 2000 + var.serial
+  rule_number    = 3000 + var.serial
   egress         = false
   protocol       = -1
   rule_action    = "allow"
@@ -9,7 +9,7 @@ resource "aws_network_acl_rule" "in_vpn_s2s" {
 
 resource "aws_network_acl_rule" "out_vpn_s2s" {
   network_acl_id = var.nacl_table
-  rule_number    = 2000 + var.serial
+  rule_number    = 3000 + var.serial
   egress         = true
   protocol       = -1
   rule_action    = "allow"
